38edcf4916fd25449e2d5ace0be7d59f9ae44960
Nandan R Pai <nandanrp09@gmail.com>
2025-02-21 19:24:18 +0530

    Update selfPass.tsx

--- src/components/alumni/selfPass.tsx
+++ src/components/alumni/selfPass.tsx
@@ -65,7 +65,7 @@ export default function SelfPass({ onClaim }: SelfPassProps) {
     const paymentObject = new (window as any).Razorpay({
       key: env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
       order_id: payment.rzpOrderID,
-      amount: 30700,
+      amount: 25600,
       currency: "INR",
       name: "Alumni Incridea",
       description: "Alumni Pass",
