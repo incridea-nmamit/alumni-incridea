a3db8d39e38206c35cff502f8e02b0ada6cceb20
shishirkarkeraa <dn.shishirgk@gmail.com>
2025-02-21 13:05:29 +0530

    changes: stylings

--- src/app/_components/pleaseLogin.tsx
+++ src/app/_components/pleaseLogin.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function PleaseLogin() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <div className="mx-4 w-full max-w-md rounded-lg border border-palate_2 bg-palate_1 p-8 text-center text-white shadow-lg">
+       <div className="mx-4 w-full max-w-md rounded-2xl  bg-blue-900 p-10 text-center text-white shadow-lg">
         <h1 className="mb-4 text-2xl font-bold">Alumni | Incridea</h1>
         <p className="text-base">
           Book your passes before coming to Incridea {"'"}25
