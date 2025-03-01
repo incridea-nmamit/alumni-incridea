0fe3f1d1293d5a59c4d72e94fb99beb1f08c495a
prabhuomkar9 <prabhuomkar9@gmail.com>
2025-03-01 13:00:50 +0530

    [fix] contact number

--- src/components/general/infoBar.tsx
+++ src/components/general/infoBar.tsx
@@ -14,9 +14,6 @@ const InfoBar = () => {
       <div className="flex w-full flex-col items-center justify-center gap-4 rounded-t-2xl bg-blue-800 p-4 md:flex-row">
         <p className="text-center text-sm text-white">
           If you have any issues with payments or anything, WhatsApp {""}
-          <a href="tel:9513295282" className="col-span-1 md:col-span-3 hover:underline hover:text-blue-500">
-            +919513295282
-          </a>{" "} or {""}
           <a href="tel:9448846524" className="col-span-1 md:col-span-3 hover:underline hover:text-blue-500">
             +919448846524
           </a>
